955f0304cb25d5f9a2f6d76417886ef96024da9a
Yash Joshi <121224573+yashjoshi5328@users.noreply.github.com>
2023-12-04 15:39:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from werkzeug.utils import secure_filename
 import os
 import cv2
 import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+from PIL import Image
 #path to upload files to
 UPLOAD_FOLDER = 'uploads'
 
@@ -421,6 +424,56 @@ def smoothening():
         return render_template('adjustment.html',image=generate_path_string(new))
     
     return render_template('adjustment.html',image=generate_path_string(new))
+
+
+
+
+def preprocess_image(image_path):
+  """ Loads image from path and preprocesses to make it model ready
+      Args:
+        image_path: Path to the image file
+  """
+  hr_image = tf.image.decode_image(tf.io.read_file(image_path))
+  # If PNG, remove the alpha channel. The model only supports
+  # images with 3 color channels.
+  if hr_image.shape[-1] == 4:
+    hr_image = hr_image[...,:-1]
+  hr_size = (tf.convert_to_tensor(hr_image.shape[:-1]) // 4) * 4
+  hr_image = tf.image.crop_to_bounding_box(hr_image, 0, 0, hr_size[0], hr_size[1])
+  hr_image = tf.cast(hr_image, tf.float32)
+  return tf.expand_dims(hr_image, 0)
+
+def save_image(image, filename):
+  """
+    Saves unscaled Tensor Images.
+    Args:
+      image: 3D image tensor. [height, width, channels]
+      filename: Name of the file to save.
+  """
+  if not isinstance(image, Image.Image):
+    image = tf.clip_by_value(image, 0, 255)
+    image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
+  image.save(filename)
+#   print("Saved as %s.jpg" % filename)
+
+@app.route('/superresolution')
+def superresolution():
+    global old
+    global new
+    
+    update_images() 
+    IMAGE_PATH = generate_path_string(new)
+    print(new)
+    SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
+
+    hr_image = preprocess_image(IMAGE_PATH)
+    model = hub.load(SAVED_MODEL_PATH)
+    fake_image = model(hr_image)
+    fake_image = tf.squeeze(fake_image)
+    update_next()
+    save_image(tf.squeeze(fake_image), filename=generate_path_string(new))
+    return render_template("layout.html",image=generate_path_string(new))
+
 
 @app.route('/about')
 def about():
